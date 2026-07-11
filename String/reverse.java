244dea60bf242c805fad547d69f91f83143e5725
Aman Kumar <amanjha411@gmail.com>
2026-07-11 23:46:59 +0530

    reverseString

---
+++ String/reverse.java
@@ -0,0 +1,20 @@
+
+
+public class reverse {
+
+    public static String reverse(String s){
+        char[] arr = s.toCharArray();
+        int start = 0;
+        int end = arr.length-1;
+        while(start < end){
+            char temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+
+return new String(arr);
+    }
+    
+}
