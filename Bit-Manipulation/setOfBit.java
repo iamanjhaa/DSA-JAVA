d88cc0b18a6b34ab692bb96b751adfd2582bd31d
Aman Kumar <amanjha411@gmail.com>
2026-07-31 13:44:26 +0530

    Add hammingWeight method in setOfBit.java
    Implement hammingWeight method to count 1 bits in an integer.

---
+++ Bit-Manipulation/setOfBit.java
@@ -0,0 +1,17 @@
+
+// also in leetcode question name is Number of 1 Bits
+
+class Solution {
+    public int hammingWeight(int n) {
+
+        int count = 0;
+
+        while(n > 0){
+            if((n& 1) != 0){
+              count++;
+            }
+          n =  n >> 1;
+        }
+        return count;
+    }
+}
